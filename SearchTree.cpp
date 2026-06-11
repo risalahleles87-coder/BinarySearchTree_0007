95c19eb39091d1ff9732148bf420b7acb81df84b
risalahlele87-coder <risalahleles87@gmail.com>
2026-06-11 15:29:28 +0700

    Menambahkan fungsi postorder pada binary tree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -109,3 +109,22 @@ public:
         }
     }
 
+    void postorder(Node *ptr)
+    {
+        // perfoms the postorder traveersal of the tree
+
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
+
