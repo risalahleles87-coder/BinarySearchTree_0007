2d855298f0a36eb54dd435ad16793671ab2683cf
risalahlele87-coder <risalahleles87@gmail.com>
2026-06-11 15:28:16 +0700

    Menambahkan fungsi inorder traversal pada binary tree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -77,3 +77,20 @@ public:
          }
     }
 
+    void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
+    
