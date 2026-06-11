fe8698c1c3a2c71e50814f91ed9e8121b8a20c74
risalahlele87-coder <risalahleles87@gmail.com>
2026-06-11 15:28:50 +0700

    Menambahkan fungsi preorder pada binary tree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -93,4 +93,19 @@ public:
         }
     }
 
-    
+    void preorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout <<"Tree is empty" <<  endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
