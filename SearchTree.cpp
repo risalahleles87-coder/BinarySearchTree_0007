5467d4c2dcf01e6ca3ff1a6b00d65defef55a1cc
risalahlele87-coder <risalahleles87@gmail.com>
2026-06-11 15:19:31 +0700

    Menambahkan atribut info, leftchild, dan rightchild pada class Node  (ulang karena typo) Dan Menambahkan constructor pada class Node

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -9,4 +9,12 @@ public:
     Node *leftchild;
     Node *rightchild;
 
-    
+    // Constructor for the node class
+    Node(string i, Node *l, Node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
